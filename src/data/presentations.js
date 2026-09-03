eebfee6eff99cdae8d7ab7d84ff2404c6eff1f03
seoyooni <choiseoyooni1016@naver.com>
2026-09-04 01:08:46 +0900

    0904

--- src/data/presentations.js
+++ src/data/presentations.js
@@ -5,12 +5,12 @@ const presentations = [
     title: '프로젝트 주제',
     date: '2026.09.09'
   },
-  {
-    id: 'week-02',
-    week: 'Week 02',
-    title: '',
-    date: ''
-  }
+  // {
+  //   id: 'week-02',
+  //   week: 'Week 02',
+  //   title: '',
+  //   date: ''
+  // }
 ];
 
 export function getPresentation(presentationId) {
